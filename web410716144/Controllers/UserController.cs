ac8d7abcf6866380d3ecf59640099eac6483b71c
410716144 <s1071614@gm.pu.edu.tw>
2021-06-03 20:58:22 +0800

    6/3

--- web410716144/Controllers/UserController.cs
+++ web410716144/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using web410716144.ViewModels;
 
 namespace web410716144.Controllers
 {
@@ -12,29 +13,29 @@ namespace web410716144.Controllers
         // GET: User
         public ActionResult SignUp()
         {
-            return View();
+            return View(new SignUpData());
         }
         [HttpPost]
-        public ActionResult SignUp(string name,string account,string password)
+        public ActionResult SignUp(SignUpData data)
         {
-            if (name.IsNullOrWhiteSpace())
+            if (data.Name.IsNullOrWhiteSpace())
             {
-                ViewBag.NameMessage = "請輸入姓名";
+                data.NameMessage = "請輸入姓名";
             }
-            if (account.IsNullOrWhiteSpace())
+            if (data.Account.IsNullOrWhiteSpace())
             {
-                ViewBag.AccountMessage = "請輸入帳號";
+               data.AccountMessage = "請輸入帳號";
             }
-            if (password.IsNullOrWhiteSpace())
+            if (data.Password.IsNullOrWhiteSpace())
             {
-                ViewBag.PasswordMessage = "請輸入密碼";
+                data.PasswordMessage = "請輸入密碼";
             }
-            if (name.IsNullOrWhiteSpace() == false &&
-                !account.IsNullOrWhiteSpace() &&
-                !password.IsNullOrWhiteSpace()) { 
-                ViewBag.Message = "註冊成功";
+            if (data.Name.IsNullOrWhiteSpace() == false &&
+                !data.Account.IsNullOrWhiteSpace() &&
+                !data.Password.IsNullOrWhiteSpace()) { 
+                 data.Message = "註冊成功";
             }
-            return View();
+            return View(data);
         }
     }
 }
