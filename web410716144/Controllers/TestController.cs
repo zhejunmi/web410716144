6a41ee84944308b5613595f8e0552825eed7fbc6
410716144 <s1071614@gm.pu.edu.tw>
2021-06-03 16:25:55 +0800

    HW1

---
+++ web410716144/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web410716144.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HW1()
+        {
+            return View();
+        }
+    }
+}
