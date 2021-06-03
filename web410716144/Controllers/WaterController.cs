4250df23cb488ef9f93b10203d3414a99e18d260
410716144 <s1071614@gm.pu.edu.tw>
2021-06-03 17:08:40 +0800

    Water

---
+++ web410716144/Controllers/WaterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web410716144.Controllers
+{
+    public class WaterController : Controller
+    {
+        // GET: Water
+        public ActionResult Index(string name, float? weight)
+        {
+            return View();
+        }
+    }
+}
