f93098e7930ed654b0ab1bcbf5a2f8248b2c1c7c
410716144 <s1071614@gm.pu.edu.tw>
2021-06-10 20:52:04 +0800

    student

--- web410716144/Controllers/UserController.cs
+++ web410716144/Controllers/UserController.cs
@@ -18,21 +18,8 @@ namespace web410716144.Controllers
         [HttpPost]
         public ActionResult SignUp(SignUpData data)
         {
-            if (data.Name.IsNullOrWhiteSpace())
-            {
-                data.NameMessage = "請輸入姓名";
-            }
-            if (data.Account.IsNullOrWhiteSpace())
-            {
-               data.AccountMessage = "請輸入帳號";
-            }
-            if (data.Password.IsNullOrWhiteSpace())
-            {
-                data.PasswordMessage = "請輸入密碼";
-            }
-            if (data.Name.IsNullOrWhiteSpace() == false &&
-                !data.Account.IsNullOrWhiteSpace() &&
-                !data.Password.IsNullOrWhiteSpace()) { 
+
+            if (ModelState.IsValid) { 
                  data.Message = "註冊成功";
             }
             return View(data);
